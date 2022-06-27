a96bf713abba1d068dd0809c1ea15a7ef70e9f81
Sakinaa782 <sashotrupa@hotmail.com>
2022-06-27 15:46:53 +0300

    JS Changes

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-const note = document.querySelector('.note');
-const buttonDelete = document.querySelector('.delete');
-const buttonEdit = document.querySelector('.edit');
+const buttonAdd = document.getElementById('add');
+
+buttonAdd.addEventListener('click', () =>addNewNote());
+
+function addNewNote(){
+    const note = document.createElement('div');
+    note.classList.add('note')
+    note.innerHTML = `<div class="note">
+    <div class="tools">
+        <button class="edit"><i class="fas fa-edit"></i>
+        </button>
+
+        <button class="delete"><i class="fa-solid fa-circle-minus"></i></button>
+        <button><i> </i></button>
+    </div>
+    <div class="main hidden"></div>
+    <textarea></textarea>
+    
+</div>  `
+    document.body.appendChild(note)
+
+
+const buttonDelete = note.querySelector('.delete');
+const buttonEdit = note.querySelector('.edit');
 const main = note.querySelector('.main');
 const textArea = note.querySelector('textarea');
 
-buttonEdit.addEventListener('click', () =>{
-    main.classList.toggle('hidden')
-    textArea.classList.toggle('hidden')
-    console.log(1);
-});
 
 textArea.addEventListener('input', (e) =>{
     const  { value }  = e.target;
@@ -16,7 +32,11 @@ textArea.addEventListener('input', (e) =>{
     main.innerHTML = value
 })
 buttonDelete.addEventListener('click', () =>{
-    main.innerHTML = "";
-    textArea.innerHTML = "";
-    
+    note.remove();
 })
+buttonEdit.addEventListener('click', () =>{
+    main.classList.toggle('hidden')
+    textArea.classList.toggle('hidden')
+    console.log(1);
+})
+};
